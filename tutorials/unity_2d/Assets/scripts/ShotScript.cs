ee480c6a5696299327bbde6e5a6d112455a5c679
trvslhlt <trvslhlt@gmail.com>
2017-04-07 11:20:29 -0400

    Menu with play button

---
+++ tutorials/unity_2d/Assets/scripts/ShotScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ShotScript : MonoBehaviour {
+
+	public int damage = 1;
+	public bool isEnemyShot = false;
+
+	void Start () {
+		Destroy (gameObject, 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveScript move = gameObject.GetComponent<MoveScript> ();
+		Debug.Log (move.direction);
+	}
+}
