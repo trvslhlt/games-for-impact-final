2c1544542cd84786e8180481cb5cec8a7eb2bd1e
trvslhlt <trvslhlt@gmail.com>
2017-04-15 19:32:06 -0400

    new level each time you play

--- projects/WinkWink_11/WinkWink_11/LevelListViewController.swift
+++ projects/WinkWink_11/WinkWink_11/LevelListViewController.swift
@@ -9,19 +9,16 @@
 import UIKit
 
 class LevelListViewController: AppViewController {
-
-    let levelNumbers = [1, 2]
-    lazy var levels: [Level] = {
-        var levels = [Level]()
-        var challenges = [TapTheVaginaNode(), SelectVulvasChallengeNode()]
-        var level = Level(number: 1, timeLimit: 20, title: "Know your body", challenges: challenges)
-        levels.append(level)
-        challenges = [TapTheVaginaNode(), TapTheVaginaNode()]
-        level = Level(number: 2, timeLimit: 20, title: "The Sex Level", challenges: challenges)
-        levels.append(level)
-        return levels
-    }()
     
+    struct LevelPreview {
+        let number: Int
+        let title: String
+    }
+    
+    let levelPreviews = [
+        LevelPreview(number: 1, title: "Know Your body"),
+        LevelPreview(number: 1, title: "The Sex Level")
+    ]
     
     @IBOutlet weak var levelListView: UITableView!
     
@@ -37,6 +34,19 @@ class LevelListViewController: AppViewController {
     @IBAction func exitTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    func getLevel(number: Int) -> Level {
+        switch number {
+        case 1:
+            let challenges = [TapTheVaginaNode(), SelectVulvasChallengeNode()]
+            return Level(number: 1, timeLimit: 20, title: "Know your body", challenges: challenges)
+        case 2:
+            let challenges = [TapTheVaginaNode(), TapTheVaginaNode()]
+            return Level(number: 2, timeLimit: 20, title: "The Sex Level", challenges: challenges)
+        default:
+            fatalError("level not found")
+        }
+    }
 }
 
 extension LevelListViewController: UITableViewDataSource, UITableViewDelegate {
@@ -44,18 +54,19 @@ extension LevelListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LevelViewController") as! LevelViewController
-        vc.level = levels[indexPath.row]
+        let preview = levelPreviews[indexPath.row]
+        vc.level = getLevel(number: preview.number)
         present(vc, animated: true, completion: nil)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return levelNumbers.count
+        return levelPreviews.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let level = levels[indexPath.row]
-        cell.textLabel?.text = "\(level.number): \(level.title)"
+        let preview = levelPreviews[indexPath.row]
+        cell.textLabel?.text = "\(preview.number): \(preview.title)"
         cell.textLabel?.textColor = Configuration.color.textPrimary
         cell.backgroundColor = .clear
         return cell
